a165cb1b487855ba41308fc9f1f3744a4e1d3772
brunoosuna8 <brunoosuna8@gmail.com>
2022-07-28 11:55:25 -0300

    changes

--- code.js
+++ code.js
@@ -8,12 +8,6 @@ close.addEventListener("click",()=>{
     document.querySelector(".menu").style.visibility = "hidden";
 })
 
-
-// para el product shop hacer 1 array de 4 arrays con 4 productos.osea 16 productos en total
-//cuando uno selecciona un producto,aparece el mismo con los 3 productos restantes de ese mismo array por debajo del producto q se le hizo click.
-//y si se hace click en los prodcutos q estan abajo en chiquito.se pone esa foto .los elementos del array van a tener la foto del producto,para poder mostrar.
-
-
 var productsContainer = document.querySelectorAll(".product-select");
 var mainProduct = document.querySelector(".product-select-show");
 
@@ -31,4 +25,12 @@ productsContainer[3].onclick = function(){
 }
 
 
+// para el product shop hacer 1 array de 4 arrays con 4 productos.osea 16 productos en total
+//cuando uno selecciona un producto,aparece el mismo con los 3 productos restantes de ese mismo array por debajo del producto q se le hizo click.
+//y si se hace click en los prodcutos q estan abajo en chiquito.se pone esa foto .los elementos del array van a tener la foto del producto,para poder mostrar.
+
+
+
+
+//sadsadsadsadsadad
 
